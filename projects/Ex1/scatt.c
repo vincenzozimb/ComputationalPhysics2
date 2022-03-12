5cb23304a1a481341f1f42412a43056daab700dd
vincenzo <vzimbardo9@gmail.com>
2022-03-12 09:35:26 +0100

    ex1 started

---
+++ projects/Ex1/scatt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <complex.h>
+#include <math.h>
+
+
+#include "util.h"
+
+int main(){
+
+
+}
---
+++ projects/Ex1/util.h
@@ -0,0 +1,8 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+/* struct */
+
+/* functions headers */
+
+#endif
