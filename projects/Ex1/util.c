918854ec4d354416bc4c1c5d788461a971edbdec
Teresa Dalle Nogare <81385841+teresadallenogare@users.noreply.github.com>
2022-03-23 18:30:44 +0100

    Bound started

--- projects/Ex1/util.c
+++ projects/Ex1/util.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 #include "util.h"
 #include "numerov.h"
@@ -20,6 +21,13 @@ double F(double r, void *par){
     return (1/xi) * ( lj(r) - E ) + (double)l*(l+1)/(r*r);
 }
 
+double Fho(double r, void *par){
+    Param *p = (Param *)par;
+    double E = p->E;
+    
+    return r * r- 2.0 * E;
+}
+
 void initialize_r(double r[], double dr, int dim){
     /* assuming the first two values of r[] to be already initialized */
     for(int i=2; i<dim; i++){
@@ -32,6 +40,52 @@ void solve_numerov(double r[], double u[], int dim, double dr, double F(double, 
     for(int i=2; i<dim; i++){
         u[i] = numerov_step(r[i],dr,u[i-1],u[i-2],F,p);
     }
+}
+
+double delta(double E, void *p){
+Param *par = (Param *)p;
+    double L = par->L;
+    double h = par->h;
+    double x0 = par->x0;
+    int n = par->n;
+    assert(n >= 0); // necessary to debug
+
+    int dimL = (int)(L+x0)/h;
+    int dimR = (int)(L-x0)/h;
+
+    double xL[dimL], xR[dimR];
+    double phiL[dimL], phiR[dimR];
+
+    par->E = E;
+
+    /* ----- Initial conditions and evolution ----- */
+    xL[0] = - L/2.0;     xL[1] = xL[0] + h;
+    xR[0] = L/2.0;     xR[1] = xR[0] - h;
+    phiL[0] = pow(-xL[0], n) * exp(- xL[0]*xL[0] /2.0);
+    phiL[1] = pow(-xL[1], n) * exp(- xL[1]*xL[1] /2.0);
+    initialize_r(xL, h, dimL);
+    solve_numerov(xL, phiL, dimL, h, Fho, par);
+
+
+    phiR[0] = pow(-xR[0], n) * exp(- xR[0]*xR[0] /2.0);
+    phiR[1] = pow(-xR[1], n) * exp(- xR[1]*xR[1] /2.0);
+    initialize_r(xR, -h, dimR);
+    solve_numerov(xR, phiR, dimR, -h, Fho, par);
+
+    /* ----- Adjust continuity ----- */
+    double R = phiL[dimL - 1]/phiR[dimR -1];
+    for (int ii = 0; ii < dimR; ii++)
+    {
+        phiR[ii] *= R;
+    }
+    
+    /* ----- Return value ----- */
+
+    double ris = phiL[dimL-2] + phiR[dimR-2];
+    ris -= (2.0 + h * h * Fho(x0, par)) * phiL[dimL-1];
+    ris /= h;
+
+    return ris;
 }
 
 double phase_shift(double r1, double r2, double u1, double u2, double k, int l, FuncBessel *f1, FuncBessel *f2){
--- projects/Ex1/util.h
+++ projects/Ex1/util.h
@@ -6,7 +6,12 @@ typedef struct Param{
     double xi;
     double E;
     int l;
+    double h;
+    double L;
+    double x0;
+    int n;
 }Param;
+
 
 typedef struct FuncBessel{
     double j_prec, j_curr, n_prec, n_curr;
@@ -17,6 +22,8 @@ typedef struct FuncBessel{
 /* functons */
 double lj(double r);
 double F(double r, void *par);
+double Fho(double r, void *par);
+double delta(double E, void *p);
 void initialize_r(double r[], double dr, int dim);
 void solve_numerov(double r[], double u[], int dim, double dr, double F(double, void*), void *p);
 double phase_shift(double r1, double r2, double u1, double u2, double k, int l, FuncBessel *f1, FuncBessel *f2);
